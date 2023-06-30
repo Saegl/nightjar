400633d7d9953b240635181212f66e21ad8f3f8b
Saegl <saegl@protonmail.com>
2023-06-30 23:52:16 +0600

    Add run options

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,26 @@ pub mod ast;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 1 {
-        println!("Provide file to execute")
+        println!("Nightjar programming language, usage: nightjar <path>")
     } else {
         let file_path = &args[1];
-        let verbose = args.contains(&"--verbose".to_string());
+        let ast_verbose = args.contains(&"--ast".to_string());
+        let dis_verbose = args.contains(&"--dis".to_string());
+        let vm_verbose = args.contains(&"--vm".to_string());
 
         let ast = parser::parse_file(file_path);
-        let co = compiler::compile_module(&ast);
-        
-        if verbose {
+        if ast_verbose {
             ast::pretty_print(&ast);
+        }
+
+        let co = compiler::compile_module(&ast);
+        if dis_verbose {
             code_obj::dis(&co);
         }
         
         let mut vm = vm::VM::new();
         vm.run(co);
-
-        if verbose {
+        if vm_verbose {
             println!("VM final state");
             vm.print_state();
         }
